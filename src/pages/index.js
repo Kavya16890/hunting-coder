ecd6f89f66733f7cb071107bbcb56340c9950c14
Kavya16890 <131844765+Kavya16890@users.noreply.github.com>
2025-05-02 12:18:49 +0530

    created a navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,43 +23,40 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script src="/main.js" strategy="lazyOnload"></Script>
+      <nav className={styles.nav}>
+        <ul className={styles.mainNav}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Content</li>
+          <li>Blog</li>
+        </ul>
+      </nav>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
           <h1 className={styles.title}>Hunting Coder</h1>
+          <p className={styles.description}>A blog for Hunting coder by a hunting coder.</p>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
+        <div className="blogs">
+          <h2 className={styles.blogh2}>Popular Blogs: </h2>
+          <div className="blogItem">
+            <h1 className={styles.blogh1}>How to learn JavaScript in 2025 ?</h1>
+            <p>JavaScript is the Language to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h1 className={styles.blogh1}>How to learn JavaScript in 2025 ?</h1>
+            <p>JavaScript is the Language to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h1 className={styles.blogh1}>How to learn JavaScript in 2025 ?</h1>
+            <p>JavaScript is the Language to design logic for web.</p>
+          </div>
+          <div className="blogItem">
+            <h1 className={styles.blogh1}>How to learn JavaScript in 2025 ?</h1> 
+            <p>JavaScript is the Language to design logic for web.</p>
+          </div>
+        </div>
       </div>
     </>
   );
