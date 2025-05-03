991add14facdf1723b2dd7af5bc8fca215ced7a3
Kavya16890 <131844765+Kavya16890@users.noreply.github.com>
2025-05-03 15:32:31 +0530

    Added server side rendering.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,33 +2,42 @@ import React, { useEffect, useState } from "react";
 import styles from "@/styles/Blog.module.css";
 import Link from "next/link";
 
-const Blog = () => {
-  const [blogs, setBlogs] = useState([])
-  useEffect(()=>{
-    fetch('http://localhost:3000/api/blogs').then((a)=>{
-      return a.json();})
-      .then((parsed)=>{
-        setBlogs(parsed)
-    })
-  }, [])
+const Blog = (props) => {
+  // console.log(props)
+  const [blogs, setBlogs] = useState(props.allBlogs);
+  // useEffect(()=>{
+
+  // }, [])
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-      <div className="blogs">
-        {blogs.map((blogItem)=>{
-          return <div className="blogItem" key={blogItem.slug}>
-            <Link href={`blogpost/${blogItem.slug}`}>
-              <h1 className={styles.blogh1}>
-                {blogItem.title}
-              </h1>
-              <p className={styles.blogItemP}>{blogItem.content.substr(0, 140)}...Read More</p>
-            </Link>
-          </div>
-        })}
+        <div className="blogs">
+          {blogs.map((blogItem) => {
+            return (
+              <div className="blogItem" key={blogItem.slug}>
+                <Link href={`blogpost/${blogItem.slug}`}>
+                  <h1 className={styles.blogh1}>{blogItem.title}</h1>
+                  <p className={styles.blogItemP}>
+                    {blogItem.content.substr(0, 140)}...Read More
+                  </p>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>
   );
 };
+
+export async function getServerSideProps( context ) {
+  let data = await fetch("http://localhost:3000/api/blogs");
+  let allBlogs = await data
+    .json()
+
+  return {
+    props: {allBlogs},
+  };
+}
 
 export default Blog;
--- src/pages/blogpost/[slug].js
+++ src/pages/blogpost/[slug].js
@@ -1,33 +1,29 @@
 import React, { useEffect, useState } from "react";
-import {useRouter} from "next/router"
+import { useRouter } from "next/router";
 import styles from "@/styles/Blog.module.css";
 
-
-const slug = () => {
-    const [blog, setBlog] = useState()
-    const router = useRouter()
-  
-  useEffect(()=>{
-    if(!router.isReady) return;
-    const {slug} = router.query;
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}.json`).then((a)=>{
-      return a.json();
-    })
-      .then((parsed)=>{
-        setBlog(parsed)
-    }) 
-  }, [router.isReady]);
-
-  return <div className={styles.container}>
-    <main className={styles.main}>
-    <h1 className={styles.blogItemh1}>{blog && blog.title}</h1>
-    <hr/>
-    <div className={styles.blogDiv}>
-  {blog && blog.content}
+const slug = (props) => {
+  const [blog, setBlog] = useState(props.myBlog);
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.blogItemh1}>{blog && blog.title}</h1>
+        <hr />
+        <div className={styles.blogDiv}>{blog && blog.content}</div>
+      </main>
     </div>
-    </main>
-  </div>;
+  );
 };
 
+export async function getServerSideProps( context ) {
+  // console.log(context.query);
+  // const router = useRouter();
+  const { slug } = context.query;
+
+let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}.json`);
+  let myBlog = await data.json();
+  return {
+    props: { myBlog },
+  };
+}
 export default slug;
- 
